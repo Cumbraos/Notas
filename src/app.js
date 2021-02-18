7688129dc62ab2939c81ce6e4137e66b359b060f
Cumbraos <cumbraosgaston15@gmail.com>
2021-02-17 22:37:22 -0300

    Falta database

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const notasRouter = require('./routes/notasRouter');
+ 
+
+app.use(express.urlencoded({ estended: false }));
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', notasRouter);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.listen(3000, function () {
+    console.log('Corriendo')
+    console.log('http://localhost:3000/')
+}) 
